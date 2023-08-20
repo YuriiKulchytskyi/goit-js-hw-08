512c898559f2bbfde3bc2655d404389f2481251e
YuriiKulchytskyi <yuriikulchytskyi1@gmail.com>
2023-08-20 20:22:35 +0300

    without second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = document.querySelector('#vimeo-player');
+const vimeo = new Player(player);
+
+const expressionKey = 'videoplayer-current-time';
+
+function saveTimeValueToLocalStorage(time) {
+  localStorage.setItem(expressionKey, JSON.stringify(time));
+}
+
+function getTimeValueFromLocalStorage() {
+  let localTime = localStorage.getItem(expressionKey);
+  if (localTime !== null) {
+    return JSON.parse(localTime);
+  }
+  return 0;
+}
+
+vimeo.setCurrentTime(getTimeValueFromLocalStorage).then(() =>
+    vimeo.on('timeupdate', throttle((
+    seconds
+  ) => saveTimeValueToLocalStorage(seconds), 1000))
+
+)
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('form')
+const email = document.querySelector('input')
+const message = document.querySelector('textarea');
+const expressionKey = 'feedback-form-state';
+
+function saveDataToLocalStorage() {
+  const formData = {
+    email: email.value,
+    message: message.value
+  }
+  localStorage.setItem(expressionKey, JSON.stringify(formData))
+}
+
+function refillDataFromLocalStorage() {
+  const localData = localStorage.getItem(expressionKey);
+
+  if (localData) {
+    const formData = JSON.parse(localData)
+    email.value = formData.email || '';
+    message.value = formData.message || '';
+  }
+}
+
+refillDataFromLocalStorage();
+
+
+email.addEventListener('input', throttle(saveDataToLocalStorage, 500));
+message.addEventListener('input', throttle(saveDataToLocalStorage, 500));
+
+
+form.addEventListener('submit', cleaner)
+
+function cleaner(event) {
+  const formData = {
+    email: email.value,
+    message: message.value
+  }
+  console.log(formData);
+
+  event.preventDefault();
+
+  email.value = '';
+  message.value = '';
+}
